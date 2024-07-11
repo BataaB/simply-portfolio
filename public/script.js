164feac31b3512f1d0b90a32abee8c2efebde1b9
Batkhuleg Bazarragchaa <khulegkhan@gmail.com>
2024-07-11 13:28:30 -0400

    added toggle with js

---
+++ public/script.js
@@ -0,0 +1,35 @@
+let isDark = false;
+
+
+function toggle() {
+  let root = document.documentElement;
+  isDark = !isDark;
+  if (!isDark) {
+    root.style.setProperty('--main-bg-color', '#E9EBEC');
+    root.style.setProperty('--btn-bg-color', 'white');
+    root.style.setProperty('--btn-color', '#3D3D3D');
+    root.style.setProperty('--profile-border-color', '#F7D039');
+    root.style.setProperty('--profile-desc-h-color', '#3D3D3D');
+    root.style.setProperty('--body-color', '#575757');
+    root.style.setProperty('--profile-fact-h-color', '#3A3A3A');
+    root.style.setProperty('--profile-btn-download-bg-color', '#FBD144');
+    root.style.setProperty('--switch-btn-color', '#19191B');
+    root.style.setProperty('--tabs-btn-portfolio', '#D7D7D7');
+  } else {
+    root.style.setProperty('--main-bg-color', '#0C151D');
+    root.style.setProperty('--btn-bg-color', '#171F26');
+    root.style.setProperty('--btn-color', '#A3ABB2');
+    root.style.setProperty('--profile-border-color', '#FFE071');
+    root.style.setProperty('--profile-desc-h-color', '#F1F2F4');
+    root.style.setProperty('--body-color', '#A3ABB2');
+    root.style.setProperty('--profile-fact-h-color', '#C3C7CB');
+    root.style.setProperty('--profile-btn-download-bg-color', '#FFE071');
+    root.style.setProperty('--profile-btn-download-color', '#3D3D3D');
+    root.style.setProperty('--switch-btn-color', '#A3ABB2');
+    root.style.setProperty('--tabs-btn-portfolio', '#0C151D');
+    root.style.setProperty('--tabs-btn-skills', 'transparent');
+  }
+  console.log('test');
+}
+
+toggle();
